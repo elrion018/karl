674b429b2552dfd653cc663a4b40b4b7333c4e92
karl <elrion018@gmail.com>
2021-10-17 02:09:07 +0900

    [feature] Rule 인터페이스 추가

--- src/CssParser.ts
+++ src/CssParser.ts
@@ -1,12 +1,26 @@
+type Value = string | number | Color;
+
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
 export interface Selector {
-  tagName: string;
-  id: string;
-  class: Array<string>;
+  tagName?: string;
+  id?: string;
+  class?: Array<string>;
+}
+
+export interface Declaraction {
+  name: string;
+  value: Value;
 }
 
 export interface Rule {
   selectors: Array<Selector>;
-  declarations: "";
+  declarations: Array<Declaraction>;
 }
 
 export class CssParser {
