91a69a66e7a46a427d96c2dff3c5310ca45451ed
karl <elrion018@gmail.com>
2021-10-17 17:04:46 +0900

    [feature] 문자 소비를 위한 메소드들 추가

--- src/CssParser.ts
+++ src/CssParser.ts
@@ -1,5 +1,9 @@
 type Value = string | number | Color;
 
+interface TestFunction {
+  (character: string): boolean;
+}
+
 export interface Color {
   r: number;
   g: number;
@@ -24,5 +28,61 @@ export interface Rule {
 }
 
 export class CssParser {
-  constructor() {}
+  input: string;
+  position: number;
+
+  constructor(input: string, position: number) {
+    this.input = input;
+    this.position = position;
+  }
+
+  parse_rules() {
+    let rules = [];
+
+    while (true) {
+      if (this.isEndOfInput()) break;
+    }
+
+    return rules;
+  }
+
+  consumeCharacter(): string {
+    const inputIterator = this.makeInputIterator(this.input, this.position);
+    const [currentPosition, currentCharacter] = inputIterator.next().value;
+    this.position += 1;
+
+    return currentCharacter;
+  }
+
+  consumeWhile(test: TestFunction): string {
+    let result = "";
+
+    while (!this.isEndOfInput() && test(this.getCharacter())) {
+      result += this.consumeCharacter();
+    }
+
+    return result;
+  }
+
+  consumeWhitespace(): void {
+    this.consumeWhile(function (character: string): boolean {
+      if (character === " ") return true;
+
+      return false;
+    });
+  }
+
+  isEndOfInput(): boolean {
+    return this.position >= this.input.length;
+  }
+
+  getCharacter(): string {
+    return this.input[this.position];
+  }
+
+  makeInputIterator = function* (input, start = 0): Generator {
+    for (let i = start; i < input.length; i++) {
+      yield [i, input[i]];
+    }
+  };
 }
